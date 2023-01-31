b1740a0e152c1a2d3a60d358076bccba05354ea4
rafiihzadi <rafiihzadi@gmail.com>
2023-01-31 14:29:49 +0700

    update

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controller;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller 
+{
+
+    public function index()
+    {
+        return view('posts',[
+            "title" => "Posts",
+            "posts" => Post::all()
+        ]);
+    }
+
+    public function show($slug)
+    {
+        return view('post',[
+            "title" => "Single Post",
+            "post" => Post::find($slug)
+
+        ]);
+    }
+}
